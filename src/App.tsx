c34506021f6286421d207e9aec0fc1215e6f314a
P1K4B00 <joshibhavya622@gmail.com>
2026-07-05 20:56:03 +0530

    feat: added idle animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 
 type CatSpriteProps = {
   row : number,
   col : number,
 }
+
+type currentState = "idle"|"typing"
 
 
 function CatSprite({
@@ -38,8 +40,20 @@ function App(){
 
   const [data,setData] = useState("");
 
+  const [currentState,setCurrentState] = useState<currentState>("idle")
+
   const [isTyping,setIsTyping] = useState(false)
   
+
+  const timeout = useRef<any>();
+  function doSomething(){
+    clearTimeout(timeout.current)
+    setCurrentState("typing");
+
+    timeout.current = setTimeout(()=>{
+      setCurrentState("idle");
+    },2000)
+  }
 
   useEffect(()=>{
     (async()=>{
@@ -77,20 +91,89 @@ function App(){
         </div>:
         null
       }
-      <CatSprite row={0} col={0} />
-      
+      <button onClick={doSomething}>mimick action</button>
+      {
+        currentState=="idle"?
+        <Idle/>:
+        <CatSprite row={0} col={0} />
+        
+      }
     </div>
   
   )
 }
 
-// function App() {
-//   return (
-//     <div style={{ color: "red", fontSize: "40px" }}>
-//       HI THERE
-//     </div>
-//   );
-// }
+function Idle(){
+
+  type idleSubState = "tailwag"|"lick"|"scratch_left"|"scratch_right"|"yawn";
+
+  const temp : Record<idleSubState,{ x:number , y:number }> = {
+   "tailwag" : {
+    y:19,
+    x : 5
+   },"lick" : {
+    y:12,
+    x : 8
+   },"scratch_left" : {
+    y:17,
+    x : 8
+   },"scratch_right" : {
+    y:18,
+    x : 8
+   },"yawn" : {
+    y:43,
+    x : 6
+   }
+  }
+  
+  const [subState,setSubstate] = useState<idleSubState>("tailwag")
+
+  const row = useMemo(()=>temp[subState].y,[subState])
+  const [col,setCol] = useState(0);
+
+
+  useEffect(()=>{
+    setSubstate("tailwag");
+
+    const int = setInterval(()=>changeSubStateRandom(),10000);
+
+
+    return ()=>clearInterval(int)
+  },[])
+
+  useEffect(()=>{
+    setCol(0)
+    
+    const int = setInterval(()=>{
+      setCol((prev)=>(prev+1)%temp[subState].x);
+    },200)
+
+    return ()=>clearInterval(int)
+  },[subState])
+
+  function changeSubStateRandom(){
+    const index : number = Math.floor(4*Math.random());
+    setSubstate((prev)=>{
+      const filteredKeys = Object.keys(temp).filter((value,index)=>{
+        if(index==0){
+          return false
+        }
+        else{
+          return true
+        }
+      })
+      const tempSubstate :idleSubState = (filteredKeys[index]) as idleSubState
+      return tempSubstate
+    })
+    setTimeout(()=>{
+      setSubstate("tailwag")
+    },2100)
+  }
+
+  return(
+    <CatSprite row={row} col={col} />
+  )
+}
 
 
 export default App
